d4b52fe05e6a871b434f06a54de3d8d0110fc23f
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-11-12 21:02:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,31 @@ class EventTarget {
     }
   
     addEventListener(event, callback) {
-      
+      this.listeners.set(event, callback);
+	  //console.log(this.listeners);
     }
   
     removeEventListener(event, callback) {
-      
+      this.listeners.delete(event);
     }
   
     dispatchEvent(event) {
-    
+		//console.log(this.listeners.get(event));
+		if(this.listeners.get(event)) {
+			this.listeners.get(event)();
+		}
     }
     
   }
-  
+
+
+const target = new EventTarget();
+const logHello = () => console.log('hello');
+const logWorld = () => console.log('world');
+target.addEventListener('hello', logHello);
+target.addEventListener('world', logWorld);
+target.dispatchEvent('hello');
+target.dispatchEvent('world');
+target.removeEventListener('hello', logHello);
+target.dispatchEvent('hello');
+target.dispatchEvent('world');
